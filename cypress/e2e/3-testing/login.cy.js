dfe0d63b84b38b4d722c65ebbe7e3648e3644063
GeniePrinie <geniexpn@gmail.com>
2023-04-04 18:25:32 +0200

    Add Login ete test

---
+++ cypress/e2e/3-testing/login.cy.js
@@ -0,0 +1,16 @@
+describe('Login and access user profile', () => {
+  it('can login successfully', () => {
+    cy.visit('http://127.0.0.1:5500/');
+    cy.wait(1000);
+    cy.get(
+      '#registerForm > div.modal-footer > button.btn.btn-outline-success'
+    ).click();
+    cy.wait(1000);
+    cy.get('#loginEmail').type('anew@stud.noroff.no');
+    cy.get('#loginPassword').type('anew12345');
+    cy.get('#loginForm > div.modal-footer > button.btn.btn-success').click();
+    cy.get('body > main > div > div.text-center.profile-actions > h4').contains(
+      'anew'
+    );
+  });
+});
